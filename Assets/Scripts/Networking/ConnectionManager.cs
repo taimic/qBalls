6ff59f9f00ba4c765ae7e696761c73f4a0197b09
unknown <dnl.rammer@gmail.com>
2016-12-08 15:22:53 +0100

    implemented tcp client; rearranged UI

---
+++ Assets/Scripts/Networking/ConnectionManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+using System;
+
+public class ConnectionManager : MonoBehaviour {
+    private delegate void RestartThread();
+
+    private static ConnectionManager instance;
+    private static bool runThread = false;
+    private static Thread clientThread;
+
+    #region unity callbacks
+    void Start() {
+        instance = this;
+
+        StartThread();
+
+        TCP_Client.FoundBall += OnFoundBall;
+    }
+
+    void OnDestroy() {
+        TCP_Client.FoundBall -= OnFoundBall;
+        clientThread.Abort();
+    }
+    #endregion
+
+    #region private
+    private void OnFoundBall(float x, float y) {
+        print("Found Ball @ x " + x + " y " + y);
+    }
+    #endregion
+
+    #region threading
+    private static void StartThread() {
+        runThread = true;
+        clientThread = new Thread(Run);
+        clientThread.Start();
+    }
+
+    public static void Run() {
+        while (runThread) {
+            TCP_Client.StartClient();
+            runThread = false;
+        }
+        // connection lost - restart thread
+        StartThread();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Networking/TCP_Client.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System;
+
+public class TCP_Client {
+    public delegate void PositionDelegate(float x, float y);
+    public static event PositionDelegate FoundBall;
+
+    public static void StartClient() {
+        bool hasConnection = false;
+        TcpClient client = null;
+        BinaryReader binReader = null;
+        while (!hasConnection) {
+            // connect to server
+            try {
+                client = new TcpClient("localhost", 4711);
+            }
+            catch {
+                Debug.Log("No server found...");
+            }
+
+            if (client != null) {
+                // read stream
+                client.ReceiveBufferSize = 8;
+                binReader = new BinaryReader(client.GetStream());
+                hasConnection = true;
+            }
+            Debug.Log("Waiting for server...");
+            Thread.Sleep(1000);
+        }
+        Debug.Log("Connection established.");
+        bool loop = true;
+        while (loop) {
+            try {
+                float x = binReader.ReadSingle();
+                float y = binReader.ReadSingle();
+
+                //Debug.Log("x " + x + " y " + y);
+                // send event
+                if (FoundBall != null)
+                    FoundBall(x, y);
+            }
+            catch (Exception) {
+                // stop loop when error occures
+                loop = false;
+            }
+        }
+        // close connection
+        client.Close();
+    }
+}
