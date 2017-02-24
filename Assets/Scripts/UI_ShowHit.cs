7527e6740e66d18d6234a1687d947886cdc26e46
unknown <dnl.rammer@gmail.com>
2017-02-24 11:26:46 +0100

    added hit particles and hit sound

--- Assets/Scripts/UI_ShowHit.cs
+++ Assets/Scripts/UI_ShowHit.cs
@@ -13,16 +13,21 @@ public class UI_ShowHit : MonoBehaviour {
     private Vector2 pos;
     private bool foundBall;
 
+    public ParticleSystem hitParticlesPrefab;
+    private AudioSource audioSource;
+
 	void Start () {
         ownRect = GetComponent<RectTransform>();
         hitMarkerImage = GetComponentInChildren<Image>();
         hitMarkerRect = hitMarkerImage.GetComponent<RectTransform>();
         hitMarkerCollider = hitMarkerImage.GetComponent<Collider2D>();
+        audioSource = GetComponent<AudioSource>();
 
         pos = new Vector2(-1000, -1000);
 
         TCP_Client.FoundBall += OnFoundBall;
 
+        StartCoroutine(TestEffects());
     }
 
     void OnDestroy() {
@@ -36,6 +41,8 @@ public class UI_ShowHit : MonoBehaviour {
             hitMarkerCollider.enabled = true;
             foundBall = false;
             StartCoroutine(DeactivateColliderDelayed());
+            ShowParticles();
+            PlaySound();
         }
     }
 
@@ -44,9 +51,28 @@ public class UI_ShowHit : MonoBehaviour {
         hitMarkerCollider.enabled = false;
     }
 
+    private void ShowParticles() {
+        ParticleSystem obj = Instantiate(hitParticlesPrefab, transform);
+        obj.transform.localPosition = hitMarkerCollider.transform.localPosition + Vector3.back*10;
+        obj.transform.localScale = Vector3.one * 0.5f;
+
+        Destroy(obj, obj.main.startLifetime.constant);
+    }
+
+    private void PlaySound() {
+        audioSource.pitch = UnityEngine.Random.Range(0.65f, 0.67f);
+        audioSource.Play();
+    }
+
     private void OnFoundBall(float x, float y) {
         pos = new Vector2(x, y);
         foundBall = true;
+    }
 
+    private IEnumerator TestEffects() {
+        for (int i = 0; i < 15; i++) {
+            yield return new WaitForSeconds(1.5f);
+            OnFoundBall(UnityEngine.Random.Range(0.1f, 0.9f), UnityEngine.Random.Range(0.2f, 0.8f));
+        }
     }
 }
