793d6dbcd98e730b9bdb4d18bdee1603c038664d
Lorenz Krautgartner <lorenz.krautgartner@gmail.com>
2017-03-07 12:45:09 +0100

    when choosing a value the last hit instruction is now shown

---
+++ Assets/ShowLastInstruction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowLastInstruction : MonoBehaviour {
+
+    public GameObject energy, sleep, move;
+
+    private void OnEnable()
+    {
+        HideInstructions();
+        if (FlowBuilder.Instance.CurrentInstruction == energy.name)
+        {
+            energy.SetActive(true);
+        }
+        else if(FlowBuilder.Instance.CurrentInstruction == sleep.name)
+        {
+            sleep.SetActive(true);
+        }
+        else if (FlowBuilder.Instance.CurrentInstruction == move.name)
+        {
+            move.SetActive(true);
+        }
+    }
+
+    private void OnDisable()
+    {
+        HideInstructions();
+    }
+
+    private void HideInstructions()
+    {
+        energy.SetActive(false);
+        sleep.SetActive(false);
+        move.SetActive(false);
+    }
+}
