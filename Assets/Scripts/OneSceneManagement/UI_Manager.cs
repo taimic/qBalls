ca9888f6839e5d9e7af160fc880356e087972d91
unknown <dnl.rammer@gmail.com>
2017-03-16 19:08:47 +0100

    single scene multi screen WIP. added UI manager. TODO: transfer logic scene change -> ui change + reload scene at the end, added build

---
+++ Assets/Scripts/OneSceneManagement/UI_Manager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Manager : Singleton<UI_Manager> {
+    public GameObject uiStart, uiCode, uiEnd, uiPlay;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToStartScreen() {
+        uiStart.SetActive(true);
+        uiCode.SetActive(false);
+        uiEnd.SetActive(false);
+        uiPlay.SetActive(false);
+    }
+
+    public void ToPlayScreen() {
+        uiStart.SetActive(false);
+        uiCode.SetActive(true);
+        uiEnd.SetActive(false);
+        uiPlay.SetActive(true);
+    }
+
+    public void ToEndScreen() {
+        uiStart.SetActive(false);
+        uiCode.SetActive(false);
+        uiEnd.SetActive(true);
+        uiPlay.SetActive(false);
+    }
+}
---
+++ Assets/UI_Screen1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Screen1 : Singleton<UI_Screen1> {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
